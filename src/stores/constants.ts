3a1adf3f48c3b30b2f79cc90debacb677a5de3aa
ElisabethSchuhmann <s2110238065@fhooe.at>
2023-10-29 18:10:44 +0100

    added expo-go can fetch from nestjs-backend

--- src/stores/constants.ts
+++ src/stores/constants.ts
@@ -3,3 +3,12 @@
 // This file contains static values used within the feature. An example of what we could store here is ACTION_TYPES data.
 
 
+export const urlLocalhost= 'https://127.0.0.1:443' //android emulator
+
+export const urlAndroid = 'https://10.0.2.2:443'
+export const urlServer = 'https://:::443'
+export const urlEliHttp = 'http://10.0.0.198:3000'
+export const urlEliHttps = 'https://10.0.0.198:443'
+export const urlRender = 'https://billify-backend.onrender.com'
+
+export const url = urlEliHttp;
