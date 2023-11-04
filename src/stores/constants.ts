8bb9f80480344df73761390d60a3fb2ba87bb213
Tobias Pfeil <tobias.pfeil@students.fh-hagenberg.at>
2023-11-04 10:33:38 +0100

    Update constants.ts

---
+++ src/stores/constants.ts
@@ -0,0 +1,17 @@
+// /constants.js
+//
+// This file contains static values used within the feature. An example of what we could store here is ACTION_TYPES data.
+
+
+//export const urlLocalhost = 'https://127.0.0.1:443' //android emulator
+
+export const urlAndroid = 'https://10.0.2.2:443'
+//export const urlServer = 'https://:::443'
+export const urlEliHttp = 'http://10.0.0.198:3000'
+export const urlEliHttps = 'https://10.0.0.198:443'
+export const urlTobiHttp = "http://192.168.137.1:3000"
+export const urlRender = 'https://billify-backend-api-srti.onrender.com'
+export const url = urlTobiHttp;
+
+export const TOKEN_KEY = "billify-jwt"
+export const USER_KEY = "billify-user_id"
