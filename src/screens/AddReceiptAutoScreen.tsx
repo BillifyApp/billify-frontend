6125d686305fac60f412d3c1712805466598d57a
Tobias Pfeil <tobias.pfeil@students.fh-hagenberg.at>
2023-11-11 15:15:15 +0100

    added delete button in receipts view

--- src/screens/AddReceiptAutoScreen.tsx
+++ src/screens/AddReceiptAutoScreen.tsx
@@ -1,120 +1,125 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import CustomSafeAreaView from "../components/CustomSafeAreaView";
-import {Pressable, Switch, Text, View} from "react-native";
+import { Pressable, ScrollView, Switch, Text, View } from "react-native";
 import ReceiptItemEntry from "../components/atom/ReceiptItemEntry";
-import {homeName, successfullyAddedName} from "../stores/route_names";
+import { homeName, successfullyAddedName } from "../stores/route_names";
 import axios from "axios";
-import {url} from "../stores/constants";
-import {useAuth} from "../context/AuthContext";
-
+import { url } from "../stores/constants";
+import { useAuth } from "../context/AuthContext";
 
 interface Item {
-    quantity: number;
-    itemName: string;
-    unitPrice: number;
+  quantity: number;
+  itemName: string;
+  unitPrice: number;
 }
 
 interface Receipt {
-    comp_name: string;
-    address: string;
-    items: Array<Item>;
-    total: number;
+  comp_name: string;
+  address: string;
+  items: Array<Item>;
+  total: number;
 }
 
 // @ts-ignore
-function AddReceiptAutoScreen({route, navigation}) {
-    const auth = useAuth().authState;
-    const {image_path, ocr_receipt: receipt} = route.params;
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+function AddReceiptAutoScreen({ route, navigation }) {
+  const auth = useAuth().authState;
+  const { image_path, ocr_receipt: receipt } = route.params;
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
+  useEffect(() => {
+    console.log(route);
+  }, [route]);
 
-    useEffect(() => {
-        console.log(route)
-    }, [route]);
-
-    const addReceipt = async () => {
-        //todo post receipt
-        let result = false
-        try {
-            result = await axios.post(
-                `${url}/receipts/add-receipt`,
-                {
-                    user_id: auth?.id,
-                    receipt: receipt,
-                    image_path: image_path
-                })
-        } catch (e) {
-            //todo print fail
-        }
-
-        if (result) {
-            navigation.navigate(successfullyAddedName)
-        }
-
-        //todo print fail
+  const addReceipt = async () => {
+    //todo post receipt
+    let result = false;
+    try {
+      result = await axios.post(`${url}/receipts/add-receipt`, {
+        user_id: auth?.id,
+        receipt: receipt,
+        image_path: image_path,
+      });
+    } catch (e) {
+      //todo print fail
     }
 
-    const dismissUpload = async () => {
-        let result = false;
-        try {
-            result = await axios.delete(
-                `${url}/images/one`,
-                {
-                    data: {
-                        image_path: image_path
-                    }
-                }
-            );
-        } catch (e) {
-            //todo print fail
-        }
-
-        if (result) {
-            navigation.navigate(homeName);
-        }
-
-        return;
-        //todo print fail
+    if (result) {
+      navigation.navigate(successfullyAddedName);
     }
 
+    //todo print fail
+  };
 
-    return (
-        <CustomSafeAreaView>
-            <Pressable onPress={dismissUpload}><Text>Back</Text></Pressable>
+  const dismissUpload = async () => {
+    let result = false;
+    try {
+      result = await axios.delete(`${url}/images/one`, {
+        data: {
+          image_path: image_path,
+        },
+      });
+    } catch (e) {
+      //todo print fail
+    }
 
-            <View style={{height: 200}}>
-                <Text>{receipt?.comp_name}</Text>
-                <Text>{receipt?.address}</Text>
-                <Text>{receipt?.date_payed}</Text>
-            </View>
-            {receipt ? receipt.items.map((i: Item, key: number) => {
-                    return (
-                        <ReceiptItemEntry
-                            count={key}
-                            key={key}
-                            itemName={i.itemName}
-                            quantity={i.quantity}
-                            unitPrice={i.unitPrice}
-                        />
-                    )
-                }
-            ) : undefined}
-            <Text>{`${receipt?.total} €`}</Text>
+    if (result) {
+      navigation.navigate(homeName);
+    }
 
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-            />
+    return;
+    //todo print fail
+  };
 
-            {isEnabled ? <View><Text>Insert groups here</Text></View> : <></>}
+  return (
+    <CustomSafeAreaView>
+      <ScrollView>
+        <Pressable onPress={dismissUpload}>
+          <Text>Back</Text>
+        </Pressable>
 
-            <Pressable onPress={addReceipt}><Text>TODO Hinzufügen</Text></Pressable>
-        </CustomSafeAreaView>
-    );
+        <View style={{ height: 200 }}>
+          <Text>{receipt?.comp_name}</Text>
+          <Text>{receipt?.address}</Text>
+          <Text>{receipt?.date_payed}</Text>
+        </View>
+        {receipt
+          ? receipt.items.map((i: Item, key: number) => {
+              return (
+                <ReceiptItemEntry
+                  count={key}
+                  key={key}
+                  itemName={i.itemName}
+                  quantity={i.quantity}
+                  unitPrice={i.unitPrice}
+                />
+              );
+            })
+          : undefined}
+        <Text>{`${receipt?.total} €`}</Text>
+
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+
+        {isEnabled ? (
+          <View>
+            <Text>Insert groups here</Text>
+          </View>
+        ) : (
+          <></>
+        )}
+
+        <Pressable onPress={addReceipt}>
+          <Text>TODO Hinzufügen</Text>
+        </Pressable>
+      </ScrollView>
+    </CustomSafeAreaView>
+  );
 }
 
 export default AddReceiptAutoScreen;
--- src/screens/OneReceiptScreen.tsx
+++ src/screens/OneReceiptScreen.tsx
@@ -1,85 +1,97 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import CustomSafeAreaView from "../components/CustomSafeAreaView";
-import {Pressable, Text, View} from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 import axios from "axios";
-import {url} from "../stores/constants";
+import { url } from "../stores/constants";
 import ReceiptItemEntry from "../components/atom/ReceiptItemEntry";
-import {homeName} from "../stores/route_names";
+import { homeName } from "../stores/route_names";
 import FlexImage from "../components/atom/FlexImage";
 
 interface Image {
-    path: string;
-    date_uploaded: string;
+  path: string;
+  date_uploaded: string;
 }
 
 interface Item {
-    quantity: number;
-    itemName: string;
-    unitPrice: number;
+  quantity: number;
+  itemName: string;
+  unitPrice: number;
 }
 
 interface Receipt {
-    _id: string;
-    user_id: string;
-    image: Image;
-    date_created: string;
-    date_payed: string;
-    comp_name: string;
-    address: string;
-    items: Array<Item>;
-    total: number;
+  _id: string;
+  user_id: string;
+  image: Image;
+  date_created: string;
+  date_payed: string;
+  comp_name: string;
+  address: string;
+  items: Array<Item>;
+  total: number;
 }
 
 // @ts-ignore
-function AddReceiptAutoScreen({route, navigation}) {
-    const { receipt_id, path } = route.params;
-    const [receipt, setReceipt] = useState<Receipt | null>(null);
+function AddReceiptAutoScreen({ route, navigation }) {
+  const { receipt_id, path } = route.params;
+  const [receipt, setReceipt] = useState<Receipt | null>(null);
 
-    useEffect(() => {
-        async function getReceipt() {
-            return await axios.post(
-                `${url}/receipts/findById`,
-                {
-                    receipt_id: receipt_id
-                }
-            ).then(res => {
-                console.log(res.data)
-                setReceipt(res.data);
-            })
-        }
+  useEffect(() => {
+    async function getReceipt() {
+      return await axios
+        .post(`${url}/receipts/findById`, {
+          receipt_id: receipt_id,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setReceipt(res.data);
+        });
+    }
 
-        getReceipt();
-        return () => {
-        };
-    }, [route]);
+    getReceipt();
+    return () => {};
+  }, [route]);
 
+  const deleteReceipt = async () => {
+    await axios.delete(`${url}/receipts/delete-one`, {
+      data: {
+        receipt_id: receipt_id,
+      },
+    });
+    navigation.navigate(homeName);
+  };
 
-    return (
-        <CustomSafeAreaView>
-            <Pressable onPress={() => navigation.navigate(homeName)}><Text>Back</Text></Pressable>
-            <Text>{receipt?._id}</Text>
-            <Text>{receipt?.image.path}</Text>
-            <View style={{height: 200}}>
-                <FlexImage path={path} width="100%" height={50}/>
-                <Text>{receipt?.comp_name}</Text>
-                <Text>{receipt?.address}</Text>
-                <Text>{receipt?.date_payed}</Text>
-            </View>
-            {receipt?.items?.map((i, key) => {
-                    return (
-                        <ReceiptItemEntry
-                            count={key}
-                            key={key}
-                            itemName={i.itemName}
-                            quantity={i.quantity}
-                            unitPrice={i.unitPrice}
-                        />
-                    )
-                }
-            )}
-            <Text>{`${receipt?.total} €`}</Text>
-        </CustomSafeAreaView>
-    );
+  return (
+    <CustomSafeAreaView>
+      <ScrollView>
+        <Pressable onPress={() => navigation.navigate(homeName)}>
+          <Text>Back</Text>
+        </Pressable>
+        <Text>{receipt?._id}</Text>
+        <Text>{receipt?.image.path}</Text>
+        <View style={{ height: 200 }}>
+          <FlexImage path={path} width="100%" height={50} />
+          <Text>{receipt?.comp_name}</Text>
+          <Text>{receipt?.address}</Text>
+          <Text>{receipt?.date_payed}</Text>
+        </View>
+        {receipt?.items?.map((i, key) => {
+          return (
+            <ReceiptItemEntry
+              count={key}
+              key={key}
+              itemName={i.itemName}
+              quantity={i.quantity}
+              unitPrice={i.unitPrice}
+            />
+          );
+        })}
+        <Text>{`${receipt?.total} €`}</Text>
+        <Pressable onPress={deleteReceipt}>
+          <Text>Delete</Text>
+        </Pressable>
+      </ScrollView>
+    </CustomSafeAreaView>
+  );
 }
 
 export default AddReceiptAutoScreen;
