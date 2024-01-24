8987be2f2a56b1fc4fa98c3dedd580467daa39a5
Tobias Pfeil <tobias.pfeil@students.fh-hagenberg.at>
2024-01-24 17:48:52 +0100

    added homescreen pictures & splash screen animation

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import * as React from "react";
 import MainContainer from "./src/containers/MainContainer";
 import { AuthProvider } from "./src/context/AuthContext";
@@ -7,17 +7,19 @@ import { AuthProvider } from "./src/context/AuthContext";
 import "./i18n";
 import * as Font from "expo-font";
 import { customFonts } from "./src/styles/fonts";
-
+import * as SplashScreen from "expo-splash-screen";
+import LottieView from "lottie-react-native";
+import { rh, rw } from "./src/utils/responsiveDimenstions";
 export default class App extends React.Component {
   state = {
     fontsLoaded: false,
+    splashScreenVisible: true,
   };
 
   async _loadFontsAsync() {
     await Font.loadAsync(customFonts);
     this.setState({ fontsLoaded: true });
   }
-
   componentDidMount() {
     this._loadFontsAsync();
   }
@@ -27,9 +29,22 @@ export default class App extends React.Component {
       return null;
     }
     return (
+      <>
+      {this.state.splashScreenVisible ? (<View style={{justifyContent: "center", alignItems: "center"}}><LottieView
+      source={require('./src/assets/animations/logo_animation.json')} // Replace with the actual path to your animation JSON file
+      autoPlay
+      style={{width: rw(110), height: rh(110)}}
+      loop={false} // Set loop to true if you want the animation to loop
+      onAnimationFinish={() => {
+        this.setState({ splashScreenVisible: false });
+        // Add any logic to navigate to your main screen or perform other actions when the animation finishes.
+      }}
+    /></View>) : (
       <AuthProvider>
-        <MainContainer />
-      </AuthProvider>
+      <MainContainer />
+    </AuthProvider>
+    )}</>
+     
     );
   }
 }
